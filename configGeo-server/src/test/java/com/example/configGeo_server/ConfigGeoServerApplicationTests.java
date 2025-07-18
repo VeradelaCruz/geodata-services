83f5e6ab85329babe1f0ac1a2fe679f0f037d175
VeradelaCruz <maidanabru@yahoo.com.ar>
2025-07-18 18:36:18 +0200

    Configurando api-gateway y configuration service

---
+++ configGeo-server/src/test/java/com/example/configGeo_server/ConfigGeoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configGeo_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigGeoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
